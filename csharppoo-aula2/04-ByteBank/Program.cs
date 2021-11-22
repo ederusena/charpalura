489fe507217711b55dcbf00c0499a1dd45434518
Eder Sena <eder.sena@live.com>
2021-11-21 23:39:43 -0300

    funcao

---
+++ csharppoo-aula2/04-ByteBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaBruno = new ContaCorrente();
+
+            contaBruno.titular = "Bruno";
+            contaBruno.Sacar(51);
+            Console.WriteLine(contaBruno.saldo);
+
+        }
+    }
+}
